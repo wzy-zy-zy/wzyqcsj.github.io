238c6510561eb00ee24df49246221a5e1db4a5fb
wzy-zy-zy <20231315036@sjzc.edu.cn>
2025-06-12 21:15:02 +0800

    Site updated: 2025-06-12 21:15:01

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/06/11/hello-world/"];function toRandomPost(){
+var posts=["2025/06/11/hello-world/","2025/05/28/我的博客/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
